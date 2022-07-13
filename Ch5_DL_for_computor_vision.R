11380b82a28539547e8c9c34b5a525ca3faa9bff
lydia126 <schung@chungbuk.ac.kr>
2022-07-13 14:26:53 +0900

    Ch5 gitignore changed for removal big data

--- Ch5_DL_for_computor_vision.R
+++ Ch5_DL_for_computor_vision.R
@@ -52,4 +52,56 @@ results
 # 2) feature extraction with a pretrained network
 # 3) fine-tuning a pretrained network 
 
+# 5.2.2 Downloading the data
+# Listing 5.4 Copying images to training, validation, and test directories
+original_dataset_dir <- "D:/GitHub/DL_with_R/data/dogs-vs-cats/train"
+base_dir <- "D:/GitHub/DL_with_R/data/cats_and_dogs_small"
+dir.create(base_dir)
+
+train_dir <- file.path(base_dir, "train")
+dir.create(train_dir)
+validation_dir <- file.path(base_dir, "validation")
+dir.create(validation_dir)
+test_dir <- file.path(base_dir, "test")
+dir.create(test_dir)
+
+train_cats_dir <- file.path(train_dir, "cats")
+dir.create(train_cats_dir)
+train_dogs_dir <- file.path(train_dir, "dogs")
+dir.create(train_dogs_dir)
+validation_cats_dir <- file.path(validation_dir, "cats")
+dir.create(validation_cats_dir)
+validation_dogs_dir <- file.path(validation_dir, "dogs")
+dir.create(validation_dogs_dir)
+test_cats_dir <- file.path(test_dir, "cats")
+dir.create(test_cats_dir)
+test_dogs_dir <- file.path(test_dir, "dogs")
+dir.create(test_dogs_dir)
+
+fnames <- paste0("cat.", 1:1000, ".jpg")
+file.copy(file.path(original_dataset_dir, fnames),
+          file.path(train_cats_dir))
+fnames <- paste0("cat.", 1001:1500, ".jpg")
+file.copy(file.path(original_dataset_dir , fnames),
+          file.path(validation_cats_dir))
+fnames <- paste0("cat.", 1501:2000, ".jpg")
+file.copy(file.path(original_dataset_dir , fnames),
+          file.path(test_cats_dir))
+fnames <- paste0("dog.", 1:1000, ".jpg")
+file.copy(file.path(original_dataset_dir , fnames),
+          file.path(train_dogs_dir))
+fnames <- paste0("dog.", 1001:1500, ".jpg")
+file.copy(file.path(original_dataset_dir , fnames),
+          file.path(validation_dogs_dir))
+fnames <- paste0("dog.", 1501:2000, ".jpg")
+file.copy(file.path(original_dataset_dir , fnames),
+          file.path(test_dogs_dir))
+cat("total training cat images:", length(list.files(train_cats_dir)), "\n")
+cat("total training dog images:", length(list.files(train_dogs_dir)), "\n")
+cat("total validation cat images:",length(list.files(validation_cats_dir)), "\n")
+cat("total validation dog images:",length(list.files(validation_dogs_dir)), "\n")
+cat("total test cat images:", length(list.files(test_cats_dir)), "\n")
+cat("total test dog images:", length(list.files(test_dogs_dir)), "\n")
+
+# 5.2.3 Building your network
 
